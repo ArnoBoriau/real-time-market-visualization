b69372517a398252059d737f75668bd49c14343d
ArnoBoriau <arno_boriau@icloud.com>
2025-10-14 18:10:11 +0200

    Update NotFound page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,18 +1,24 @@
 import { Title } from "@solidjs/meta";
 import { HttpStatusCode } from "@solidjs/start";
+import { useNavigate } from "@solidjs/router";
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <main>
-      <Title>Not Found</Title>
+    <main class="notfound_main">
+      <Title>Page Not Found</Title>
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        Sorry, we couldn't find the page you're looking for.
+        <br />
+        <button
+          class="btn btn-primary btn-notfound"
+          onClick={() => navigate("/")}
+        >
+          Return to Dashboard
+        </button>
       </p>
     </main>
   );
